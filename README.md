30c081d6a4309498793647e1e17742cb96cf5a0c
Faikor Kastrioti <fajkorkastrioti@gmail.com>
2022-03-08 20:15:15 +0100

    First commit for rock-paper-scissors project. But,it's not finished yet. I still have to work on game() function and to also fix a 'buggy' behaviour that playRoundfunction is displaying.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rock-paper-scissors
+
+This is the third project of the Odin Project carriculum. In this project we, the learners, are coding a Rock, paper, scsissors game. For the moment the game will have no graphical interface. One will be able to play the game in the JavaScript console. However, a little bit later in the carriculum we will add a graphical interface as well.
+
+Two players will play the game. Human and computer.
+
+To code this game I will need to use functions, conditionals, for loop, and a couple of JavaScript methods. 
---
+++ script.js
@@ -0,0 +1,135 @@
+/* I need a function to allow the computer to return one of the three options: `Rock`, `Paper` or `Scissors`. */
+function computerPlay() {
+  let computerInput = Math.random(0, 0.9999);
+  if (computerInput < 0.3333 /* The more floating point I have, the more accurate the result of computerPlay function should be*/ ) {
+    return `Rock`;
+  } else if (computerInput < 0.6666 ) {
+    return `Paper`;
+  } else if (computerInput < 0.9999) {
+      return `Scissors`;
+  }
+}
+
+/* I want a funtion that makes the first letter of the user input capital while the rest of them remain lower case. */
+function firstLetterCapital(userInput) {
+    let lowerCase = userInput.slice(1).toLowerCase();
+    let upperCase = userInput.slice(0,1).toUpperCase();
+    return `${upperCase}${lowerCase}`;
+}
+
+/* I need a function to allow the user to input `Rock`, `Paper` or `Scissors`. */
+function humanPlay() {
+  let userInput = prompt(`Please type in 'Rock', 'Paper' or 'Scissors'`);
+  
+  if (userInput === `Rock` || userInput === `Paper` || userInput === `Scissors`) {
+    return userInput;
+  } else { /* I assume here that the user has either entered the right input but not with the first letter capital, or has given a wrong input alltogether. */
+    userInput = firstLetterCapital(userInput);
+    if (userInput === `Rock` || userInput === `Paper` || userInput === `Scissors`) {
+      return userInput;
+    } else {
+      userInput = prompt(`Wrong input! Please, try again!`);
+      if (userInput === `Rock` || userInput === `Paper` || userInput === `Scissors`) {
+        return userInput;
+      } else { /* The same assumption as above. */
+        userInput = firstLetterCapital(userInput);
+        if (userInput === `Rock` || userInput === `Paper` || userInput === `Scissors`) {
+          return userInput;
+        }else {
+          return undefined;
+        }
+
+      }
+    }
+  }
+} 
+
+/* I need a funtion to decide the winner when human and computer play one round together. */
+function playRound(playerSelection, computerSelection) {
+  playerSelection = humanPlay();
+  
+  if (playerSelection === undefined) {
+    return undefined;
+  } 
+
+  computerSelection = computerPlay();
+  console.log(playerSelection);
+  console.log(computerSelection);
+
+  if (playerSelection === `Rock`) {
+    if (computerSelection === `Paper`) {
+      return `Computer won round!`;
+    } else if (computerSelection === `Scissors`) {
+      return `Human won round!`;
+    } else {
+      return `Draw!`;
+    } 
+  } else if (playerSelection === `Paper`) {
+      if (computerSelection === `Scissors`) {
+        return `Computer won round!`;
+      } else if (computerSelection === `Rock`) {
+        return `Human won round!`;
+      }else {
+        return `Draw!`;
+      }
+  } else {
+      if (computerSelection === `Rock`) {
+        return `Computer won round!`;
+      } else if (computerSelection === `Paper`) {
+        return `Human won round!`;
+      } else {
+        return `Draw!`;
+      }
+  }
+}
+
+/* I need a function to decide and print the winner in the console when human and computer play five rounds together.  */
+  function game() {
+  let playRoundResult = playRound();
+
+  if (playRoundResult === undefined) {
+    alert(`Wrong input!`);
+    alert(`Not possible to play game!`);
+    alert(`Terminating game...`)
+    return;
+  }
+   
+  let humanScore = 0;
+  let computerScore = 0;
+  
+  for (let i = 0; i < 5; i++) {
+  playRoundResult = playRound();
+    if (playRoundResult === `Computer won round!`) {
+      computerScore++;
+      console.log(`Human ${humanScore} - ${computerScore} Computer`);
+    } else if (playRoundResult === `Human won round!`) {
+      humanScore++;
+      console.log(`Human ${humanScore} - ${computerScore} Computer`);
+    } else if (playRoundResult === `Draw!`) {
+      console.log(`Human ${humanScore} - ${computerScore} Computer`);
+    }
+  }
+} 
+
+
+/* I need to test computerPlay() to see if it returns `Rock`, `Paper` and `Scissors` the same amount of times. */
+/******* Testing computerPlay() *******/
+let rock = 0;
+let paper = 0;
+let scissors = 0;
+for (let i = 0; i < 1000000; i++) {
+  if (computerPlay() === `Rock`) {
+    rock++;
+  } else if (computerPlay() === `Paper`) {
+    paper++;
+  } else {
+    scissors++;
+  }
+}
+
+console.log(`Rock: ${rock}`);
+console.log(`Paper: ${paper}`);
+console.log(`Scissors ${scissors}`);
+
+/*Unfortunately computerPlay() is not very accurate. According to the test computerPlay() most of the times it return numbers from the upper range (between 0.6666 and 0.9999) */
+
