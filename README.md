8f98763f1002d95564ce80c109d903c6baf09c8c
Faikor Kastrioti <fajkorkastrioti@gmail.com>
2022-04-15 15:51:27 +0200

    Add style.css and reset.css, modify index.html, script.js
    * index.html
    I added a container with a 'h1' header and a button.
    
    * style.css
    Added some basic style while I am working on the script.js
    
    * script.js
    I commented out the previous version of the code and started working on
    the graphical UI of the game. So far, I have added some basic
    interactivity to it. More is to come.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # rock-paper-scissors
 
-This is the third project of the Odin Project carriculum. In this project we, the learners, are coding a Rock, paper, scsissors game. For the moment the game will have no graphical interface. One will be able to play the game in the JavaScript console. However, a little bit later in the carriculum we will add a graphical interface as well.
+This is the third project of the Odin Project curriculum. In this project we, the learners, are coding a Rock, paper, scissors game. For the moment the game will have no graphical interface. One will be able to play the game in the JavaScript console. However, a little bit later in the curriculum we will add a graphical interface as well.
 
 Two players will play the game. Human and computer.
 
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* A function to allow the computer to return randomly one of these 3 options: `Rock`, `Paper` or `Scissors`. */
-function computerPlay() {
+/* function computerPlay() {
   let computerInput = Math.random(0, 0.9999);
   if (computerInput < 0.3333) {
     return `Rock`;
@@ -8,16 +8,16 @@ function computerPlay() {
   } else if (computerInput < 0.9999) {
       return `Scissors`;
   }
-}
+} */
 
 /* A function to allow the user to input `Rock`, `Paper` or `Scissors`. */   
-function humanPlay() {
+/* function humanPlay() {
   let userInput = prompt(`Please type in 'Rock', 'Paper' or 'Scissors'`);
   return userInput;
-} 
+}  */
 
 /* A function to decide the winner when human and computer play one round together. */
-function playRound(playerSelection, computerSelection) {
+/* function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === `Rock`) {
     if (computerSelection === `Paper`) {
@@ -44,10 +44,10 @@ function playRound(playerSelection, computerSelection) {
         return `Draw!`;
       }
   }
-}
+} */
 
 /* A function to output in the console the score and either the winner of the game or the winner of the round. */
-function game() {
+/* function game() {
 
   let humanScore = 0;
   let computerScore = 0;
@@ -87,10 +87,10 @@ function game() {
         } 
     }
     startOver();
-}
+} */
 
 /* This function allows us to play another game with computer */
-function startOver() {
+/* function startOver() {
   let startOverGame = prompt(`Do you want to play another game?`, `Answer with 'Yes' or 'No'`);
   if (startOverGame === `Yes`) {
     game();
@@ -98,4 +98,85 @@ function startOver() {
     alert(`Game Over!`);
     return;
   }
+} */
+
+const container = document.querySelector(`.container`);
+const h1 = document.querySelector(`h1`);
+const startGame = document.querySelector(`.startGame`);
+const instructionText = document.createElement(`p`); /* A paragraph ('p') with an instruction text */
+instructionText.classList.add(`instructionText`);
+startGame.addEventListener(`click`, giveInstructions);
+
+function giveInstructions() {
+  /* Removes 'h1' and startGame from container and adds instructionText to it */
+  removeContainerChildren(); 
+  /* Show instructions before game start */
+  instructionText.textContent = `You will play against the computer`; 
+  setTimeout(showTextTwo, 2000);
+  setTimeout(showTextThree, 5000);
+  setTimeout(showTextFour, 7000);
+  setTimeout(showTextFive, 8500)
+  /* Show the 3 'Rock', 'Paper', 'Scissors' buttons */
+  setTimeout(addPlayerSelection, 8500);
 }
+
+/* Removes 'h1' and startGame from container and adds instructionText to it */
+function removeContainerChildren() {
+  container.removeChild(h1);
+  container.removeChild(startGame);
+  container.appendChild(instructionText);
+}
+
+/* Shows second instruction text */
+function showTextTwo() {
+  instructionText.textContent = `For every round that you win, you get 1 point`; 
+}
+
+/* Shows third instruction text */
+function showTextThree() {
+  instructionText.textContent = `You need five points to win game`; 
+}
+
+/* Show fourth instruction text */
+function showTextFour() {
+  instructionText.textContent = `Are Ready To Play?`; 
+}
+
+/* Shows fifth instruction text */
+function showTextFive() {
+  instructionText.textContent = `Please Select!`;
+}
+
+/* A function to show the 3 'Rock', 'Paper', 'Scissors' buttons */
+function addPlayerSelection() {
+  
+  const playerSelection = document.createElement(`div`); /* A container of the 3 buttons */
+  const rock = document.createElement(`button`);
+  const paper = document.createElement(`button`);
+  const scissors = document.createElement(`button`);
+
+  playerSelection.classList.add(`playerSelection`);
+
+  rock.classList.add(`rock`);
+  rock.textContent = `Rock`;
+  playerSelection.appendChild(rock); /* Append Rock button to playerSelection */
+
+  paper.classList.add(`paper`);
+  paper.textContent = `Paper`;  
+  playerSelection.appendChild(paper); /* Append Paper button to playerSelection */
+
+  scissors.classList.add(`scissors`);
+  scissors.textContent = `Scissors`;
+  playerSelection.appendChild(scissors); /* Append Scissors button to playerSelection */
+
+  container.appendChild(playerSelection); /* Append playerSelection to container div */
+
+  // rock.addEventListener(`click`, )
+}
+
+
+
+
+
+
+
